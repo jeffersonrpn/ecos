021e6035609d8f992c692dd88b400b249c1f9939
Jefferson Neves <jefferson.rpn@gmail.com>
2025-04-06 16:44:18 -0300

    Add índice nas fraes

--- crawler/run.js
+++ crawler/run.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const URL = "https://www.pensador.com/365_frases_motivacionais/";
-const OUTPUT_FILE = path.join(__dirname, "../../public/quotes.csv");
+const OUTPUT_FILE = path.join(__dirname, "../public/quotes.csv");
 
 async function fetchQuotes() {
   try {
@@ -12,17 +12,25 @@ async function fetchQuotes() {
     const $ = cheerio.load(data);
     const quotes = [];
 
+    let baseDate = new Date();
+    let index = 1;
+
     $("blockquote").each((index, element) => {
       const quote = $(element).find("p").first().text().trim();
       const author = $(element).find("p.autor").text().trim();
       if (quote) {
-        if (author) {
-          quotes.push(
-            `"${quote.replace(/"/g, '""')}","${author.replace(/"/g, '""')}"`
-          );
-        } else {    
-          quotes.push(`"${quote.replace(/"/g, '""')}","--"`);
-        }
+        const timestamp = new Date(baseDate);
+        timestamp.setDate(baseDate.getDate() + index);
+        const formattedDate = timestamp
+          .toISOString()
+          .slice(0, 10)
+          .replace(/-/g, "");
+        const q = quote.replace(/"/g, "").replace(/\n/g, "<br>");
+        const entry = author
+          ? `"${q}","${author.replace(/"/g, "")}"`
+          : `"${q}","--"`;
+        quotes.push(`${index},${formattedDate},${entry}`);
+        index++;
       }
     });
     // Criar diretório se não existir
@@ -31,7 +39,8 @@ async function fetchQuotes() {
     }
 
     // Escrever no arquivo CSV
-    fs.writeFileSync(OUTPUT_FILE, '"Frase"\n' + quotes.join("\n"), "utf8");
+    const header = "id,index,quote,author\n";
+    fs.writeFileSync(OUTPUT_FILE, header + quotes.join("\n"), "utf8");
     console.log(`Arquivo salvo em: ${OUTPUT_FILE}`);
   } catch (error) {
     console.error("Erro ao buscar as frases:", error.message);
