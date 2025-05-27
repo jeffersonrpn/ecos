86f6e8ce9ed23df1c339cfafb57defba7bb30c1e
Jefferson Neves <jefferson.neves@vtex.com>
2025-05-27 11:58:42 -0300

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,20 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: "Ecos",
-  description: "Frases inspiradoras para o seu dia",
+  description: "Frases inspiradoras. Todos os dias.",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicon/apple-touch-icon.png', type: 'image/png', sizes: '180x180' },
+      { url: '/favicon/android-chrome-192x192', type: 'image/png', sizes: '192x192' },
+      { url: '/favicon/android-chrome-512x512', type: 'image/png', sizes: '512x512' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180' },
+    ],
+  },
 };
 
 export default function RootLayout({
